c0fc3e81f0a4a57e76f7e6336fdc2b9ad0815e16
AntonioSalces <antoniosalces06@gmail.com>
2025-03-02 22:55:53 +0100

    02/03/25

---
+++ Programación/Java/Ficheros/Repaso examen recu/src/Cano_GestorFlamenco/FlamencoArchivo.java
@@ -0,0 +1,66 @@
+package Cano_GestorFlamenco;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class FlamencoArchivo {
+
+    public FlamencoArchivo(String file) {
+        createFile(file);
+    }
+
+    public File createFile(String name) {
+        File file = new File(name);
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return file;
+    }
+
+    public void writeFile(String name, String phrase) {
+        createFile(name);
+        try {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(name, true));
+            bw.write(phrase);
+            bw.newLine();
+            bw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    public ArrayList<String> readFile(String name) {
+        ArrayList<String> phrases = new ArrayList<>();
+        String linea = "";
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(name));
+            while ((linea = br.readLine()) != null) {
+                phrases.add(linea);
+            }
+            br.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return phrases;
+    }
+
+    public void deleteFile(String name) {
+        File file = new File(name);
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+}
---
+++ Programación/Java/Ficheros/Repaso examen recu/src/Cano_GestorFlamenco/GestorFlamenco.java
@@ -0,0 +1,7 @@
+package Cano_GestorFlamenco;
+
+import java.util.ArrayList;
+
+public class GestorFlamenco {
+    private ArrayList<String> phrases;
+}
